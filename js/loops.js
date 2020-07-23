34a6fb8a0ae21c97e72ddbb2a078a11ef7c809bb
Ishita Golchha <ishita.golchha@zensar.com>
2020-07-23 23:30:49 +0530

    Add new assignment

---
+++ js/loops.js
@@ -0,0 +1,14 @@
+console.log('While Loop: ')
+let x=0
+while(x!==10){
+console.log(`x= ${x}`)
+x++
+} 
+
+console.log('Do While Loop: ')
+
+x=20
+do{
+console.log(`x= ${x}`)
+x--
+}while(x!==0);
---
+++ js/switch.js
@@ -0,0 +1,21 @@
+console.log("Switch:  ")
+
+let number=2
+
+switch(number){
+    case 1:
+        console.log("Case 1")
+        break
+    case 2:
+        console.log("Case 2")
+        break
+    case 3:
+        console.log("Case 3")
+        break
+    case 4:
+        console.log("Case 4")
+        break
+    default:
+        console.log("Case 5")
+        break
+}
